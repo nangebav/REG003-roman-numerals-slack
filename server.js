cb660945fc917145feca61c877c042d8815631e4
nangebav <nangebav@gmail.com>
2021-09-14 21:28:58 -0500

    make it better

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send('hola')
+})
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('está escuchando el puerto');
+})
